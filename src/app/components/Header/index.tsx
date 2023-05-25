3ac1ce511f763286935d1cc6e0047d43e477f5d8
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-05-25 13:55:22 -0300

    style: HTML Landmarks

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -6,7 +6,13 @@ import Image from 'next/image'
 export function Header() {
   return (
     <header className="flex justify-between items-center pt-5">
-      <Image src="/rocketseat-logo.svg" width={150} height={100} alt="logo" />
+      <Image
+        src="/rocketseat-logo.svg"
+        width={150}
+        height={150}
+        alt="logo"
+        className="object-cover"
+      />
       <nav>
         <a
           href="https://github.com/viniciusferreira7"
